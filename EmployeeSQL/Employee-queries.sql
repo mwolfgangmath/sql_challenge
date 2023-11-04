84a8a05cd25fb83b50a89f9a6888ac9313e8958b
mwolfgangmath <mwolfgangmath@gmail.com>
2023-11-04 17:04:38 -0500

    Diagram and Schema completed

--- EmployeeSQL/Employee-queries.sql
+++ EmployeeSQL/Employee-queries.sql
@@ -1,2 +1,33 @@
 --Module 9 Challenge
 --Record Queries for the employee questions here
+
+--Q1 Employee number, last name, first name, sex, and salary
+
+
+
+--Q2 first name, last name, adn hire date for the employees hired in 1986
+
+
+
+--Q3 manager of each dpartmennt along with their dept#, dept name, exployee#, last name adn first name
+
+
+
+--Q4 department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+
+
+--Q5 first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+
+
+--Q6 List each employee in the Sales department, including their employee number, last name, and first name.
+
+
+
+--Q7 List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
--- EmployeeSQL/Employee-schema.sql
+++
@@ -1,2 +0,0 @@
---Module 9 HW
---Set up Table Schemata here
